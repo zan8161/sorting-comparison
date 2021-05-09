cb3ca5f0a56153b5ed76db6b54c188001b8079ec
zan8161 <75531215+zan8161@users.noreply.github.com>
2021-05-09 21:52:32 +0800

    Delete qsor.h

--- qsor.h
+++
@@ -1,6 +0,0 @@
-void SWAP(int *data_1, int *data_2);
-void SWAPforStrings(char *string1, char *string2);
-int partition(int *number, int left, int right);
-int partitionforStrings(char **strings, int left, int right);
-void quickSort(int *number, int left, int right);
-void quickSortforStrings(char **strings, int left, int right);
